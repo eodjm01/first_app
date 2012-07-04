f7a21c8642971f0abb7f3a9418d10f052380039a
David McNally <mcnally.david@gmail.com>
2012-07-04 11:50:30 -0400

    Improve the readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby on Rails Tutorial: First Application
+
+First App
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 FirstApp::Application.routes.draw do
+  get "static_pages/about"
+
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
